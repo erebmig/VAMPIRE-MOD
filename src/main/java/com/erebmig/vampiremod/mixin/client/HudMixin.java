4db925632fe2a2d2eabeccbaa30f2af213ed33fa
erebmig <boorapolat@gmail.com>
2026-08-11 19:46:54 +0300

    Add HUD mixin

---
+++ src/main/java/com/erebmig/vampiremod/mixin/client/HudMixin.java
@@ -0,0 +1,8 @@
+package com.erebmig.vampiremod.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(net.minecraft.client.gui.hud.InGameHud.class)
+public class HudMixin {
+    // HUD modifications handled in VampireRenderManager
+}
